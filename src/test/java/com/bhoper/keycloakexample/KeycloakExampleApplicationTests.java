e130b8ce45d007fa4805c3833d5f326427c21444
Andrey Solianyk <solianik123456@gmail.com>
2024-06-18 13:20:49 +0200

    root commit

---
+++ src/test/java/com/bhoper/keycloakexample/KeycloakExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhoper.keycloakexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
